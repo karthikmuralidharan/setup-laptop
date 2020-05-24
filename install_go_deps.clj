3ce62993a50c569fef9efebcef234e9550e7b372
Karthik Muralidharan <mkarthik91@gmail.com>
2020-05-24 13:05:08 +0530

    init commit

---
+++ install_go_deps.clj
@@ -0,0 +1,30 @@
+#!/usr/bin/env bb
+
+(def go-global-tools [
+    "github.com/mdempsky/gocode"
+    "github.com/uudashr/gopkgs/v2/cmd/gopkgs"
+    "github.com/ramya-rao-a/go-outline"
+    "github.com/acroca/go-symbols"
+    "golang.org/x/tools/cmd/guru"
+    "golang.org/x/tools/cmd/gorename"
+    "github.com/cweill/gotests/..."
+    "github.com/fatih/gomodifytags"
+    "github.com/josharian/impl"
+    "github.com/davidrjenni/reftools/cmd/fillstruct"
+    "github.com/haya14busa/goplay/cmd/goplay"
+    "github.com/godoctor/godoctor"
+    "github.com/go-delve/delve/cmd/dlv"
+    "github.com/stamblerre/gocode"
+    "github.com/rogpeppe/godef"
+    "golang.org/x/tools/cmd/goimports"
+    "golang.org/x/lint/golint"
+    "golang.org/x/tools/gopls"
+])
+
+(defn install-go-tools []
+    (doseq [tool go-global-tools]
+        (println "Installing" tool)
+        (shell/sh "go" "get" tool))
+    (println "go tools installed successfully"))
+
+(install-go-tools)
